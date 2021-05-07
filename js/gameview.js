2abb41362117c090669e55f39464f750c621c9b7
Arinatumblr <arinusik.arina@gmail.com>
2021-05-07 22:36:47 +0000

    Tile color

--- js/gameview.js
+++ js/gameview.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d')
 const d = 3
 const tileWidth = canvas.width / d
 const tileHeight = canvas.height / d
-const tileColor = '#484749f'
+const tileColor = '#484749'
 const canvasColor = '#6a6d70'
 canvas.style.backgroundColor = canvasColor
 
